ba9550207792c2ad6fa99f51fbe0b90e8d9f9032
gabriel-fz <gabrielfzancanela@gmail.com>
2022-04-17 15:02:06 -0300

    adicionando mais configurações

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started!');
+});
